d4ff6b97b8aef63df37f3696537415658aca6dd9
Impolite Goofball <github.dismiss894@passmail.net>
2025-09-17 21:03:17 +0930

    Create a file parser to parse a file into a specified format

---
+++ src/main/java/net/ethandankiw/parser/file/FileParser.java
@@ -0,0 +1,9 @@
+package net.ethandankiw.parser.file;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileParser {
+
+	private static final Logger logger = LoggerFactory.getLogger(FileParser.class);
+}
