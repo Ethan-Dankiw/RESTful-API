bfe522477eafdb27ed214e726901fa5c0ac4ddb0
Impolite Goofball <github.dismiss894@passmail.net>
2025-09-17 21:03:39 +0930

    Create http parsers for response and request for inter-socket communication

---
+++ src/main/java/net/ethandankiw/parser/http/HttpRequestParser.java
@@ -0,0 +1,12 @@
+package net.ethandankiw.parser.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.ethandankiw.parser.file.FileParser;
+
+public class HttpRequestParser {
+
+	private static final Logger logger = LoggerFactory.getLogger(HttpRequestParser.class);
+
+}
---
+++ src/main/java/net/ethandankiw/parser/http/HttpResponseParser.java
@@ -0,0 +1,12 @@
+package net.ethandankiw.parser.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.ethandankiw.parser.file.FileParser;
+
+public class HttpResponseParser {
+
+	private static final Logger logger = LoggerFactory.getLogger(FileParser.class);
+
+}
